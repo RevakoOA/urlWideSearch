3bf5993ab732a843e88b77dc349083f3cefaa6e7
wRevakoOA <wRevakoOA@gmail.com>
2019-05-08 10:39:10 +0300

    initial commit with empty activity

---
+++ app/src/main/java/com/just_me/urlwidesearch/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.just_me.urlwidesearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
